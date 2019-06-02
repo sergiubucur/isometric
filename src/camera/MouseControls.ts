fb936334d5d07661159b1128a6e497840aa85bf9
Sergiu Bucur <sergiu.bucur@outlook.com>
2019-06-02 23:37:04 +0300

    add movement

--- src/camera/MouseControls.ts
+++ src/camera/MouseControls.ts
@@ -6,6 +6,8 @@ import IInputTracker from "../input-tracker/IInputTracker";
 import IScene from "../scene/IScene";
 import ILogger from "../logger/ILogger";
 
+const Speed = 0.25;
+
 export default class MouseControls implements ICameraControls {
 	private readonly _camera: ICamera;
 	private readonly _inputTracker: IInputTracker;
@@ -13,6 +15,9 @@ export default class MouseControls implements ICameraControls {
 	private readonly _logger: ILogger;
 
 	private _position: THREE.Vector3;
+	private _velocity: THREE.Vector3;
+	private _target: THREE.Vector3;
+	private _steps: number;
 	private _mesh: THREE.Mesh;
 	private _pointerMesh: THREE.Mesh;
 	private _pointLight: THREE.PointLight;
@@ -25,6 +30,9 @@ export default class MouseControls implements ICameraControls {
 		this._logger = logger;
 
 		this._position = new THREE.Vector3(16, 0, 16);
+		this._velocity = new THREE.Vector3();
+		this._target = new THREE.Vector3();
+		this._steps = 0;
 		this._plane = new THREE.Plane(new THREE.Vector3(0, 1, 0));
 
 		this._camera.setPosition(this._position);
@@ -35,14 +43,22 @@ export default class MouseControls implements ICameraControls {
 
 	update() {
 		this.updateMousePosition();
+		this.updateZoomLevel();
 
-		let zoomVelocity = this._inputTracker.wheelEvents.reduce((a, x) => a + x, 0);
-		if (zoomVelocity !== 0) {
-			this._camera.setZoom(this._camera.zoom + (zoomVelocity * 2));
+		if (this._steps > 0) {
+			this._position.add(this._velocity);
+			this._camera.setPosition(this._position);
+			this.updateMeshPosition();
+
+			this._steps--;
+			if (this._steps === 0) {
+				this._position = this._target;
+			}
 		}
 
 		this._logger.logVector3("position", this._position);
 		this._logger.logNumber("zoom", this._camera.zoom);
+		this._logger.logNumber("steps", this._steps);
 	}
 
 	private updateMousePosition() {
@@ -55,20 +71,25 @@ export default class MouseControls implements ICameraControls {
 		const position = new THREE.Vector3();
 		raycaster.ray.intersectPlane(this._plane, position);
 
-		const gridPosition = new THREE.Vector2(
+		const mousePosition = new THREE.Vector2(
 			Math.round(position.x),
 			Math.round(position.z)
 		);
 
-		this._pointerMesh.position.set(gridPosition.x, 0.05, gridPosition.y);
-		this._logger.logVector2("gridPosition", gridPosition);
+		this._pointerMesh.position.set(mousePosition.x, 0.05, mousePosition.y);
+		this._logger.logVector2("gridPosition", mousePosition);
 
 		if (this._inputTracker.leftMouseDown) {
-			this._position.x = gridPosition.x;
-			this._position.z = gridPosition.y;
+			this._target = new THREE.Vector3(mousePosition.x, 0, mousePosition.y);
+			this._velocity.copy(this._target).sub(this._position).normalize().multiplyScalar(Speed);
+			this._steps = Math.ceil(this._target.clone().sub(this._position).length() / Speed);
+		}
+	}
 
-			// this._camera.setPosition(this._position);
-			this.updateMeshPosition();
+	private updateZoomLevel() {
+		let zoomVelocity = this._inputTracker.wheelEvents.reduce((a, x) => a + x, 0);
+		if (zoomVelocity !== 0) {
+			this._camera.setZoom(this._camera.zoom + (zoomVelocity * 2));
 		}
 	}
 
