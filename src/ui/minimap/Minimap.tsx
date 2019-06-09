9cd53eef311cbd01994aa8dfba7c4f45b54a0a8e
Sergiu Bucur <sergiu.bucur@outlook.com>
2019-06-09 10:26:41 +0300

    refactoring

--- src/ui/minimap/Minimap.tsx
+++ src/ui/minimap/Minimap.tsx
@@ -11,74 +11,106 @@ interface Props {
 	logger: ILogger
 }
 
-const Size = 32;
-const CellSize = 8;
-const RefreshRateMs = Math.floor(1 / 30 * 1000);
+type Color = {
+	r: number,
+	g: number,
+	b: number
+};
+
+const Size = 48;
+const CellSize = 6;
 
 // TODO: reimplement using canvas or something faster
 export default class Minimap extends Component<Props> {
-	private _intervalId: NodeJS.Timeout;
-	private _ref: RefObject<HTMLDivElement>;
+	private _animationFrameId: number;
+	private _canvasRef: RefObject<HTMLCanvasElement>;
+	private _ctx: CanvasRenderingContext2D;
 
 	constructor(props: Props) {
 		super(props);
 
-		this._ref = React.createRef<HTMLDivElement>();
+		this._canvasRef = React.createRef<HTMLCanvasElement>();
 	}
 
 	componentDidMount() {
-		this._intervalId = setInterval(() => {
-			const time = performance.now();
-
-			this.renderCells();
-
-			this.props.logger.logBounds("renderCells", performance.now() - time);
-		}, RefreshRateMs);
+		this.draw();
 	}
 
 	componentWillUnmount() {
-		clearInterval(this._intervalId);
+		cancelAnimationFrame(this._animationFrameId);
+	}
+
+	private draw() {
+		this._animationFrameId = requestAnimationFrame(() => this.draw());
+		const time = performance.now();
+
+		this.renderCells();
+
+		this.props.logger.logBounds("renderCells", performance.now() - time);
 	}
 
 	private renderCells() {
 		const { world, player } = this.props;
 
-		const playerPosition = world.map.convertToMapPosition(player.position);
-		const cells = Array.from(this._ref.current.children) as HTMLDivElement[];
+		if (!this._ctx) {
+			this._ctx = this._canvasRef.current.getContext("2d", { alpha: false });
+		}
 
-		let k = 0;
+		const playerPosition = world.map.convertToMapPosition(player.position);
+		const imageData = this._ctx.getImageData(0, 0, Size, Size);
+		const color = { r: 0, g: 0, b: 0 };
+
 		for (let z = 0; z < Size; z++) {
 			for (let x = 0; x < Size; x++) {
 				let x0 = playerPosition.x - Size / 2 + x;
 				let z0 = playerPosition.z - Size / 2 + z;
 
 				const cell = world.map.getCell(x0, z0);
-				let background = "#404040";
+				color.r = 64;
+				color.g = 64;
+				color.b = 64;
 
 				if (!cell) {
-					background = "#000";
+					color.r = 0;
+					color.g = 0;
+					color.b = 0;
 				} else {
 					if (cell.type === CellType.Void) {
-						background = "#fff";
+						color.r = 255;
+						color.g = 255;
+						color.b = 255;
 					} else {
 						const id = world.map.occupiedCells[z0][x0];
 
 						if (id === player.id) {
-							background = "#bada55";
+							color.r = 186;
+							color.g = 218;
+							color.b = 85;
 						} else {
 							if (id !== player.id && id !== 0) {
-								background = "#ff0000";
+								color.r = 255;
+								color.g = 0;
+								color.b = 0;
 							}
 						}
 					}
 				}
 
-				const div = cells[k];
-				div.style.background = background;
-
-				k++;
+				this.setPixel(imageData, x, z, color);
 			}
+
+			this._ctx.putImageData(imageData, 0, 0);
 		}
+	}
+
+	private setPixel(imageData: ImageData, x: number, y: number, color: Color) {
+		const data = imageData.data;
+		const i = (y * Size + x) * 4;
+
+		data[i] = color.r;
+		data[i + 1] = color.g;
+		data[i + 2] = color.b;
+		data[i + 3] = 0;
 	}
 
 	render() {
@@ -95,14 +127,24 @@ export default class Minimap extends Component<Props> {
 			borderBottom: "1px solid #808080"
 		};
 
+		const canvasStyles: any = {
+			width: Size * CellSize,
+			height: Size * CellSize,
+			imageRendering: "pixelated"
+		};
+
 		const cells = [];
 		for (let i = 0; i < Size * Size; i++) {
 			cells.push(true);
 		}
 
 		return(
-			<div ref={this._ref} style={styles}>
-				{cells.map((x, i) => <div key={i} style={{ width: CellSize, height: CellSize }}></div>)}
+			<div style={styles}>
+				<canvas
+					ref={this._canvasRef}
+					width={Size}
+					height={Size}
+					style={canvasStyles}></canvas>
 			</div>
 		);
 	}
