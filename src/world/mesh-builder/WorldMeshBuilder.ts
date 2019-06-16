67fdea19e00ab208a51c6ac88dba14437edbe594
Sergiu Bucur <sergiu.bucur@outlook.com>
2019-06-16 23:58:50 +0300

    modify todo

--- src/world/mesh-builder/WorldMeshBuilder.ts
+++ src/world/mesh-builder/WorldMeshBuilder.ts
@@ -47,7 +47,7 @@ export default class WorldMeshBuilder implements IWorldMeshBuilder {
 			rootMesh.add(floorMesh);
 		});
 
-		// TODO: remove walls that are facing away from the camera
+		// TODO: remove occluded walls
 		edges.forEach(edge => {
 			const geometry = this.getWallGeometry(edge);
 			const bufferGeometry = new THREE.BufferGeometry().fromGeometry(geometry);
