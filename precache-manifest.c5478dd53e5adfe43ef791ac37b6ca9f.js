3bac2707a9ed8c028c2261b0095efde50b1b28de
Sergiu Bucur <sergiu.bucur@outlook.com>
2019-06-26 20:09:26 +0300

    Updates

---
+++ precache-manifest.c5478dd53e5adfe43ef791ac37b6ca9f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78df10826babfa6ae08e8c496e6c1f98",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e3d4a1fd741f8a72392",
+    "url": "./static/css/2.013955c8.chunk.css"
+  },
+  {
+    "revision": "a31bc6a4bdb58da2430e",
+    "url": "./static/css/main.8128de5b.chunk.css"
+  },
+  {
+    "revision": "9e3d4a1fd741f8a72392",
+    "url": "./static/js/2.8bb19d91.chunk.js"
+  },
+  {
+    "revision": "a31bc6a4bdb58da2430e",
+    "url": "./static/js/main.a3a82fad.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "bc54396771c64863193f7a3fe93d9cff",
+    "url": "./static/media/rpgawesome-webfont.bc543967.ttf"
+  },
+  {
+    "revision": "c243adb601cb0e965344851b1c46194f",
+    "url": "./static/media/rpgawesome-webfont.c243adb6.svg"
+  },
+  {
+    "revision": "cbfd0298f008558557418d19abfc2f42",
+    "url": "./static/media/rpgawesome-webfont.cbfd0298.eot"
+  },
+  {
+    "revision": "e31a701aa9b88125d36119acb8a10482",
+    "url": "./static/media/rpgawesome-webfont.e31a701a.woff"
+  }
+]);
